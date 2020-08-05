4f6857b98043f0c6f1e2cb269601aa377650b710
hackers87 <66948701+hackers87@users.noreply.github.com>
2020-08-05 16:09:01 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -6,10 +6,10 @@ TOKEN = ''#place your token
 @bot.event
 async def on_ready():
     while True:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming,name="Welcome Members!"))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming,name="SELLING QUIPP AND HQ NUMBER "))
         await asyncio.sleep(5)
 
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming,name="$help"))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming,name="LIFE AND RECHARGE 1250RS 1 YEAR"))
         await asyncio.sleep(5)
 
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=f'''{len(bot.guilds)} servers'''))
