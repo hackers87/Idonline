f98d120c59260cda42db19b543d7d5de50c3e3d6
hackers87 <66948701+hackers87@users.noreply.github.com>
2020-08-07 15:16:14 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="")#select your prefix
+
 TOKEN = 'NjQwMTUxMjI1OTgzMTcyNjA4.XxAJYQ.vHzi-udBKKZSqzaeUwChUP-61OE'#place your token
 @bot.event
 async def on_ready():
@@ -12,8 +12,8 @@ async def on_ready():
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming,name="LIFE AND RECHARGE 1250RS 1 YEAR"))
         await asyncio.sleep(5)
 
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=f'''{len(bot.guilds)} servers'''))
-        await asyncio.sleep(5)
+
+
         print("READY !\n{0.user.name}\n{0.user.id}".format(bot))
 
 bot.run(TOKEN, bot=False)
